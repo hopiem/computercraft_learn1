21f54bfb9a215b3015b6baaab0e9a2749d77a5d0
hopiem <32547584+hopiem@users.noreply.github.com>
2017-10-05 18:25:58 +0200

    OszlopFel függvény a hiányzó oszlop miatt
    Lottéval nehezen beizzadva

--- em.lua
+++ em.lua
@@ -25,18 +25,48 @@ function EpitekFalat(magas, szeles)
   end
 end
 
-function EpitekHazat ()
-  for i=1,3 do 
-    EpitekFalat(3,5)
-    turtle.forward()
+function EpitekUtsoFalat(magas, szeles)
+  turtle.select(1)
+  
+  for i=1,magas do
+	for o=1,szeles do
+	  turtle.back()
+	  turtle.place()
+	end
+
 	turtle.turnRight()
-    turtle.down()
-    turtle.down()
-    turtle.down()
-   end
-   EpitekFalat(3,4)
+	turtle.turnRight()
+
+	turtle.up(1)
+    turtle.back()
+  end
+end
+
+function EpitekHazat()
+	for i=1,3 do 
+		EpitekFalat(3,5)
+		turtle.forward()
+		turtle.turnRight()
+		turtle.down()
+		turtle.down()
+		turtle.down()
+	end
+	
+	EpitekFalat(3,4)
+	
+	turtle.forward()
+	for i=1,3 do
+		turtle.down()
+	end
+	OszlopFel(3)
+end
+
+function OszlopFel (magas)
+	for i=1,magas do
+		turtle.up ()
+		turtle.placeDown ()
+	end	
 end
 
 EszekHaKell(100)
 EpitekHazat()
-
