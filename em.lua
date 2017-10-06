3ea13fc1b9c0bbb4db50e504b5995fdd4caaeb9f
hopiemi <32547584+hopiem@users.noreply.github.com>
2017-10-06 15:15:03 +0200

    OszlopFel már nem kell Emi Anyával megoldotta :)

--- em.lua
+++ em.lua
@@ -42,25 +42,6 @@ function EpitekUtsoFalat(magas, szeles)
   end
 end
 
-function EpitekHazat()
-	for i=1,3 do 
-		EpitekFalat(3,5)
-		turtle.forward()
-		turtle.turnRight()
-		turtle.down()
-		turtle.down()
-		turtle.down()
-	end
-	
-	EpitekFalat(3,4)
-	
-	turtle.forward()
-	for i=1,3 do
-		turtle.down()
-	end
-	OszlopFel(3)
-end
-
 function OszlopFel (magas)
 	for i=1,magas do
 		turtle.up ()
@@ -68,5 +49,26 @@ function OszlopFel (magas)
 	end	
 end
 
+function EpitekHazat(magas, szeles)
+	for i=1,2 do 
+		EpitekFalat(magas, szeles)
+		turtle.forward()
+		turtle.turnRight()
+		for j=1,magas do
+			turtle.down()
+		end
+	end
+	
+	for i=1,2 do
+		EpitekFalat(magas,szeles+1)
+		turtle.forward()
+		turtle.turnRight()
+		for j=1,magas do
+			turtle.down()
+		end
+	end
+end
+
+
 EszekHaKell(100)
-EpitekHazat()
+EpitekHazat(3, 3)
