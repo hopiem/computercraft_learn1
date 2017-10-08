efbc3500464b4ab6f5a5e61367245e416fa8548b
hopiemi <32547584+hopiem@users.noreply.github.com>
2017-10-08 18:13:40 +0200

    using util.lua

--- simple_digging/NuttyOne.lua
+++ simple_digging/NuttyOne.lua
@@ -1,11 +1,16 @@
+
+local util = dofile("util.lua")
+
 function GoDown(times)
-  for i=1, times do
-    turtle.dig()
-    turtle.forward()
-    turtle.digUp()
-    turtle.digDown()
-    turtle.down()
-  end
+	for i=1, times do
+		turtle.dig()
+		turtle.forward()
+		turtle.digUp()
+		turtle.digDown()
+		turtle.down()
+	end
 end
 
-GoDown(10)
+util.hello()
+
+-- GoDown(10)
