16b6a52d4fd63d7d042b2aa87b819efe2efa6ef5
hopiemi <32547584+hopiem@users.noreply.github.com>
2017-10-08 18:14:15 +0200

    initial

---
+++ simple_digging/util.lua
@@ -0,0 +1,19 @@
+local util = {}
+
+function util.hello()
+	print("Hello from util :)")
+end
+
+function util.select(name)
+	for i=1, 16 do
+		info = turtle.getItemDetail(i)
+		
+		if info then
+			if info.name == name then
+				turtle.select(i)
+			end
+		end
+	end
+end
+
+return util
