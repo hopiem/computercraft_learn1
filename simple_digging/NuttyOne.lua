5fe36f08d5970066e0965e2a40cc41a685f710e6
hopiemi <32547584+hopiem@users.noreply.github.com>
2017-10-08 18:48:16 +0200

    go down till get the bedrock, after that...
    go straight forward (not handle the gravel, place too many torch than that )

--- simple_digging/NuttyOne.lua
+++ simple_digging/NuttyOne.lua
@@ -1,16 +1,44 @@
 
 local util = dofile("util.lua")
+util.hello()
 
 function GoDown(times)
+	local bedrock = false
+	
 	for i=1, times do
 		turtle.dig()
 		turtle.forward()
+		
+		see, block = turtle.inspect()
+		if see and block.name == "minecraft:bedrock" then
+			bedrock = true
+			
+			print("Elertem az aljat :)")
+			-- break
+			
+			turtle.up()
+		end
+		
 		turtle.digUp()
 		turtle.digDown()
-		turtle.down()
+		
+		if i % 8 == 0 then
+			PlaceTorch()
+		end
+		
+		if not bedrock then
+			turtle.down()
+		end
 	end
 end
 
-util.hello()
+function PlaceTorch()
+	util.turn()
+	
+	util.select("minecraft:torch")
+	turtle.place()
+	
+	util.turn()
+end
 
--- GoDown(10)
+GoDown(100)
