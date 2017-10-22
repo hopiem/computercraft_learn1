67129a2fd56aaf0290365e374d333316e068856e
hopiemi <32547584+hopiem@users.noreply.github.com>
2017-10-22 10:39:57 +0200

    egy csomó hasznos került bele :)

--- simple_digging/util.lua
+++ simple_digging/util.lua
@@ -1,7 +1,7 @@
 local util = {}
 
 function util.hello()
-	print("Hello from util :)")
+	print("Hello from util :) v0.3")
 end
 
 function util.select(name)
@@ -20,9 +20,67 @@ function util.select(name)
 	return false
 end
 
+function util.printItems(name)
+	for i=1, 16 do
+		info = turtle.getItemDetail(i)
+		
+		if info then
+			print(info.name)
+		end
+	end
+end
+
+function util.lookAround()
+	local look = {}
+	
+	for i=1,4 do
+		lookInfo(i, look, turtle.inspect())
+		turtle.turnRight()
+	end
+	
+	lookInfo("up", look, turtle.inspectUp())
+	lookInfo("down", look, turtle.inspectUp())
+	
+	util.printTable(look)
+	
+	return look
+end
+
+function util.printTable(look)
+	for key, value in pairs(look) do 
+		print(key .. "," .. value)
+	end
+end
+
+function lookInfo(tag, look, see, info)
+	if see then
+		print(tag .. ": " .. info.name)
+		look[tag] = info.name
+	end
+end
+
 function util.turn()
 	turtle.turnLeft()
 	turtle.turnLeft()
+end
+
+function util.dig(info)
+	digInfo(info, turtle.dig())
+end
+function util.digUp(info)
+	digInfo(info, turtle.digUp())
+end
+function util.digDown(info)
+	digInfo(info, turtle.digDown())
+end
+
+function digInfo(info, ok, msg)
+	if not info then
+		info = ""
+	end
+	if not ok then
+		print("Nem sikerült felfele ásni " .. msg .. " / " .. info)
+	end
 end
 
 return util
